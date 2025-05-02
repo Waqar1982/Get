d577c8e4146a93891e98ecc1dcbaa9552dcda553
Waqar1982 <waquar82@gmail.com>
2025-05-02 14:21:32 +0200

    a

--- CodeAlong/script.js
+++ CodeAlong/script.js
@@ -1,6 +1,12 @@
 function updateview(){
-app.innerHTML=`
-<input onchange = "saveName(this.value)">
+app.innerHTML=/*HTML*/`
+
+      <input onchange = "saveName(this.value)"type="text" placeholder="Navn">
+      <input onchange = "saveAge (this.value)"type="number" placeholder="Alder">
+      <button>Legg til barn</button>
+      <ul></ul>
+      <button>Sjekk skolestartere</button>
+      <div id="resultat"></div>
 `
 
 }
@@ -8,4 +14,8 @@ app.innerHTML=`
 function saveName(name){
 inputName= name
 
+}
+
+function saveAge(age) {
+    inputAge= age
 }
